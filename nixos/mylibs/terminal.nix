7816d55682293945e92ad9dae80101bd77a19e87
volkswagenfeature <13547477+volkswagenfeature@users.noreply.github.com>
2022-11-06 23:50:34 +0000

    initial commit

---
+++ nixos/mylibs/terminal.nix
@@ -0,0 +1,29 @@
+{lib, pkgs, config, ...}:
+with lib;
+{
+  environment.systemPackages = with pkgs; [
+    oh-my-fish
+    tree
+    git
+    
+    # man pages packages
+    man-db
+    man-pages
+    man-pages-posix
+
+    # Shell assist
+    nix-index
+
+
+
+
+
+  ];
+
+  documentation.dev.enable = true;
+  # Of questionable neccesity? I'm leaving it commented out for now.
+  # See https://nixos.wiki/wiki/Apropos  
+  documentation.man.generateCaches = true; 
+
+
+}
