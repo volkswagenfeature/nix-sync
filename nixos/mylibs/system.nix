63bd9318e9c7c20a4d3c9d3c97075259f8af6cbf
volkswagenfeature <13547477+volkswagenfeature@users.noreply.github.com>
2022-11-08 15:44:07 -0500

    Split up default configuration.nix

--- nixos/mylibs/system.nix
+++ nixos/mylibs/system.nix
@@ -9,13 +9,38 @@ with lib;
   # ssh-agent config
   programs.ssh.startAgent = true;
 
+  ### Networking ##
+  networking.hostName = "BetaBlue-NixOS-2022"; # Define your hostname.
 
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Enable network manager applet
+  programs.nm-applet.enable = true;
+
+  ### Internationilization/Input ###
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.utf8";
+
+  ### User configs ###
+
+  users.users.tristan = {
+    isNormalUser = true;
+    description = "tristan";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      firefox
+    #  thunderbird
+    ];
+  };
 
   # Homemanager configs
   # users.users.nixos.isNormalUser = true;
   # home-manager.users.nixos = {pkgs, ...}:{
   #  programs.fish.enable = true;
-
   # };
 
 
