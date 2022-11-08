7bde23ffc23250184bfd29342ad2ae048cf76964
tristan BetaBlue-NixOS-2022 <tristan@BetaBlue-NixOS-2022.com>
2022-11-08 15:44:07 -0500

    Split up default configuration.nix

---
+++ nixos/mylibs/gui/xserver.nix
@@ -0,0 +1,44 @@
+{lib,pkgs,config, ... }:
+with lib;
+{
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the LXQT Desktop Environment.
+  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.desktopManager.lxqt.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+
+
+  ### Audio ###
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+
+
+
+
+}
