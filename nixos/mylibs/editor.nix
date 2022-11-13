64a09b08a80e119b70ed996d90dac9df348156ed
volkswagenfeature <13547477+volkswagenfeature@users.noreply.github.com>
2022-11-13 17:32:41 -0500

    Fixed tabs

--- nixos/mylibs/editor.nix
+++ nixos/mylibs/editor.nix
@@ -3,7 +3,7 @@ with lib;
 {
     environment.systemPackages = with pkgs; [ 
     ((vim_configurable.override { }).customize {
-       name = "vim";	
+       name = "vim";    
        vimrcConfig = {
 
        beforePlugins = ''
@@ -14,41 +14,41 @@ with lib;
            "Compat options
            set nocompatible
 
-	   "Default tab settings
-	   set smartindent
-	   set tabstop=4 shiftwidth=4 expandtab softtabstop=4
+           "Default tab settings
+           set smartindent
+           set tabstop=4 shiftwidth=4 expandtab softtabstop=4
 
-	   "Savefile options:
+           "Savefile options:
 
 
-	   "Interface specifics
-  	   set ruler
-	   set number
-	   set background=dark
-	   set hlsearch
-	   set smartindent
-	   syntax enable
+           "Interface specifics
+           set ruler
+           set number
+           set background=dark
+           set hlsearch
+           set smartindent
+           syntax enable
 
        '';
        };
 
        vimrcConfig.packages.customize = with pkgs.vimPlugins; {
-	   start = [
-	   #Completion
+           start = [
+           #Completion
            rainbow 
-	   vim-repeat 
+           vim-repeat 
            vim-surround 
            lexima-vim
 
-	   #Navigation
-	   vim-signature 
+           #Navigation
+           vim-signature 
            ctrlp 
            nerdtree
            #Syncronization
            ];
            
 
-	   
+           
 
        }; 
     })
