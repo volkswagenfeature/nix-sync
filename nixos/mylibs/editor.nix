38bb405420312830e0aeb9f3830b768e9642e044
tristan BetaBlue-NixOS-2022 <tristan@BetaBlue-NixOS-2022.com>
2022-11-15 10:44:54 -0500

    Fixed indentation

--- nixos/mylibs/editor.nix
+++ nixos/mylibs/editor.nix
@@ -1,57 +1,53 @@
 {lib, pkgs, config, ...}:
 with lib;
 {
-    environment.systemPackages = with pkgs; [ 
-    ((vim_configurable.override { }).customize {
-       name = "vim";    
-       vimrcConfig = {
+  environment.systemPackages = with pkgs; [ 
+      ((vim_configurable.override { }).customize {
+        name = "vim";    
+        vimrcConfig = {
 
-       beforePlugins = ''
-           set nocompatible
-           filetype plugin indent on
-       '';
-       customRC = ''
-           "Compat options
-           set nocompatible
+          beforePlugins = ''
+               set nocompatible
+               filetype plugin indent on
+          '';
+          customRC = ''
+               "Compat options
+               set nocompatible
 
-           "Default tab settings
-           set smartindent
-           set tabstop=4 shiftwidth=4 expandtab softtabstop=4
+               "Default tab settings
+               set smartindent
+               set tabstop=4 shiftwidth=4 expandtab softtabstop=4
 
-           "Savefile options:
+               "Savefile options:
 
 
-           "Interface specifics
-           set ruler
-           set number
-           set background=dark
-           set hlsearch
-           set smartindent
-           syntax enable
+               "Interface specifics
+               set ruler
+               set number
+               set background=dark
+               set hlsearch
+               set smartindent
+               syntax enable
 
-       '';
-       };
+          '';
+        };
 
-       vimrcConfig.packages.customize = with pkgs.vimPlugins; {
-           start = [
-           #Completion
-           rainbow 
-           vim-repeat 
-           vim-surround 
-           lexima-vim
+        vimrcConfig.packages.customize = with pkgs.vimPlugins; {
+          start = [
+            #Completion
+            rainbow 
+            vim-repeat 
+            vim-surround 
+            lexima-vim
 
-           #Navigation
-           vim-signature 
-           ctrlp 
-           nerdtree
-           #Syncronization
-           ];
-           
-
-           
-
-       }; 
-    })
+            #Navigation
+            vim-signature 
+            ctrlp 
+            nerdtree
+            #Syncronization
+          ];
+        }; 
+      })
   ];
   programs.vim.defaultEditor = true;
 }
