952000c63f8aecda8d6f23e5c64a9217decd5249
tristan BetaBlue-NixOS-2022 <tristan@BetaBlue-NixOS-2022.com>
2022-12-18 13:31:39 -0500

    Added any-nix-shell and oh-my-fish

--- nixos/mylibs/terminal.nix
+++ nixos/mylibs/terminal.nix
@@ -2,7 +2,7 @@
 with lib;
 {
   environment.systemPackages = with pkgs; [
-    #oh-my-fish
+    oh-my-fish
     fish
     tree
     git
@@ -14,6 +14,7 @@ with lib;
 
     # Shell assist
     nix-index
+    any-nix-shell
 
 
 
@@ -23,6 +24,10 @@ with lib;
 
   documentation.dev.enable = true;
   programs.fish.enable = true;
+  programs.fish.promptInit = ''
+    any-nix-shell fish --info-right | source
+  '';
+
   # Of questionable neccesity? I'm leaving it commented out for now.
   # See https://nixos.wiki/wiki/Apropos  
   #documentation.man.generateCaches = true; 
