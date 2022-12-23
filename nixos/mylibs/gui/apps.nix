08a71f3c2e0af019c481a0d7785a4b43ba675787
volkswagenfeature <13547477+volkswagenfeature@users.noreply.github.com>
2022-12-23 13:27:42 -0500

    Moved GUI apps to GUI folder

--- nixos/mylibs/gui/apps.nix
+++ nixos/mylibs/gui/apps.nix
@@ -1,8 +1,31 @@
 {lib,pkgs,config, ... }:
 with lib;
 {
+  users.users.tristan.packages = with pkgs; [
+    # Networking
+    firefox
+
+    # Social media
+    discord
+    element-desktop
+
+    # Design
+    graphviz
+    super-slicer
+
+    # Utilities
+    kitty
+    obsidian
+
+    # image processing
+    imagemagick
+    feh
+  ];
+
   environment.systemPackages = with pkgs; [
 	  kitty
 	  feh
 	 ];
 }
+
+
