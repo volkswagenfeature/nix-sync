f0a8302e1b4a5f314bb39a905863a0b4263f0d7d
volkswagenfeature <13547477+volkswagenfeature@users.noreply.github.com>
2023-05-14 14:57:08 -0400

    added highlighting/autocomplete support, added GUI apps.

--- nixos/mylibs/gui/apps.nix
+++ nixos/mylibs/gui/apps.nix
@@ -26,7 +26,18 @@ in
 
     # Password managment
     keepassxc
+
+    # Office
+    libreoffice-qt
+    hunspell
+    hunspellDicts.en_US
   ];
+
+  fonts.fonts = with pkgs; [
+    noto-fonts
+    #noto-fonts-extra
+  ];
+
 
   environment.systemPackages = with pkgs; [
 	  kitty
