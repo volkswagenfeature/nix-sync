15736052e74d4a0d82d8ffd682226e4948d6339e
volkswagenfeature <13547477+volkswagenfeature@users.noreply.github.com>
2023-09-17 21:33:54 -0400

    updated disc uuid

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -29,24 +29,36 @@ in
     };
 */
   # SD card busted filesystems.
+  /*
+
+/dev/sda4: UUID="b6267e25-5b59-47eb-9477-38920ea74512" UUID_SUB="8dc5f03d-82a0-473e-99c6-8759ad29df06" BLOCK_SIZE="4096" TYPE="btrfs" PARTUUID="842f8f13-12aa-3c46-8063-358154c2b7ff"
+/dev/sda2: SEC_TYPE="msdos" UUID="962A-5F50" BLOCK_SIZE="512" TYPE="vfat" PARTUUID="411c6214-dd89-1541-8ff3-2a7103f4b65e"
+/dev/sda5: UUID="1982d770-324c-4bc6-aa79-1f54aa18ec42" BLOCK_SIZE="4096" TYPE="ext4" PARTLABEL="root" PARTUUID="40f88ab3-8871-3441-9bfd-fc4cbaa007cf"
+/dev/sda3: UUID="1694b659-3d72-440c-8a58-e4277660d78a" TYPE="swap" PARTUUID="8cd364a7-88b8-4a4f-bc8f-58795accac52"
+/dev/sda1: UUID="7FC7-0A23" BLOCK_SIZE="512" TYPE="vfat" PARTLABEL="EFI-SYSTEM" PARTUUID="319b3741-7bc8-ba45-94ba-dc9acfb402da"
+/dev/sdb: UUID="ffd43f69-4797-4598-a1d9-c6aa6a0c30d0" TYPE="crypto_LUKS"
+/dev/mapper/enc: UUID="863256c3-63d8-4530-abf0-af5e2fc3f96d" UUID_SUB="2cf6473d-117b-439c-822d-1945287874cf" BLOCK_SIZE="4096" TYPE="btrfs"
+*/
+
   fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/863256c3-63d8-4530-abf0-af5e2fc3f96d";
+    { device = "/dev/disk/by-uuid/b6267e25-5b59-47eb-9477-38920ea74512";
       fsType = "btrfs";
-      options = ["subvol=home"];
+      options = ["subvol=home" ];
     };
 
   fileSystems."/home/${secrets.primaryuser}/bulk" =
-    { device = "/dev/disk/by-uuid/863256c3-63d8-4530-abf0-af5e2fc3f96d";
+    { device = "/dev/disk/by-uuid/b6267e25-5b59-47eb-9477-38920ea74512";
       fsType = "btrfs";
-      options = ["subvol=bulk"];
+      options = ["subvol=bulk" ];
     };
-    
+/*
+  # broken.
   fileSystems."/home/${secrets.primaryuser}/bulk/dropbox" =
     { device = "/dev/disk/by-uuid/863256c3-63d8-4530-abf0-af5e2fc3f96d";
       fsType = "btrfs";
       options = ["subvol=dropbox"];
     };
-
+*/
 
 
   boot.initrd.luks.devices."enc" = 
