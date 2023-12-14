07d9fefc21a81cf237e133d85c843dc50fdd7027
volkswagenfeature <2@2.2>
2023-12-14 10:20:19 -0500

    Writing mkDerivation for backlight script

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, inputs, nixpkgs, lib, ... }:
+{ config, pkgs, inputs, nixpkgs, lib , ... }:
 with builtins;
 let
   # autodetect = (x: (builtins.trace x x)) 
