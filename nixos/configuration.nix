89bd13e89c0ee0736126ba63848b0b5a821dea0d
volkswagenfeature <13547477+volkswagenfeature@users.noreply.github.com>
2023-12-17 20:09:28 -0500

    Moved boot variables to hardware-configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -27,24 +27,8 @@ in
     experimental-features = nix-command flakes
   '';
 
-
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/efi";
-
   # Enable CUPS to print documents.
   services.printing.enable = true;
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
-
-
-   # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  #system.stateVersion = "23.11"; # Did you read the comment?
-
 }
