cdd3ad10f768603ab5cec7b28491618efdc24e30
volkswagenfeature <13547477+volkswagenfeature@users.noreply.github.com>
2024-02-13 12:08:59 -0500

    New version of media-keys works now

--- nixos/mylibs/gui/media-keys/media-keys.nix
+++ nixos/mylibs/gui/media-keys/media-keys.nix
@@ -12,38 +12,40 @@ let
 
   ### Package definitions ###
 
-  brightness = stdenv.mkDerivation rec {
-      pname = "brightness";
+  brightness = pkgs.stdenv.mkDerivation rec {
+      name = "brightness";
       inherit wobfifo blpath steps blmax;
 
       src = ./.;
 
       configurePhase= ''
         mkdir -p $out/bin
-        cp -t $out/bin/ brightness_arr.sh 
-        chmod +x $out/bin/brightness_arr.sh
+        cp -t $out/bin/ ./brightness.sh 
+        chmod +x $out/bin/brightness.sh
+        
         SPECIAL=$( ./generator.sh )
-        substituteInPlace $out/bin/brightness_arr.sh \
-          --replace "wobfifo"  "$wobfifo"\
+        substituteInPlace $out/bin/brightness.sh \
+          --replace "wob-fifo"  "$wobfifo"\
           --replace bl-path   "$blpath"\
           --replace val-array "$SPECIAL"
       '';
     };  
 
-    vol-control = pkgs.writeshellscriptbin "vol-control" ''
-      eval "${pkgs.pamixer}/bin/pamixer -$1 10"
-      if [[ $(${pkgs.pamixer}/bin/pamixer --get-mute) = "true" ]] &&\
-         [[ $(${pkgs.pamixer}/bin/pamixer --get-volume) -ne 0 ]] 
-      then
-        echo $(($(${pkgs.pamixer}/bin/pamixer --get-volume)+199)) > ${wobfifo}
+  vol-control = pkgs.writeShellScriptBin "vol-control" ''
+    eval "${pkgs.pamixer}/bin/pamixer -$1 10"
+    if [[ $(${pkgs.pamixer}/bin/pamixer --get-mute) = "true" ]] &&\
+       [[ $(${pkgs.pamixer}/bin/pamixer --get-volume) -ne 0 ]] 
+    then
+      echo $(($(${pkgs.pamixer}/bin/pamixer --get-volume)+199)) > ${wobfifo}
 
-      else
-        ${pkgs.pamixer}/bin/pamixer --get-volume > ${wobfifo}
-      fi
-    '';
+    else
+      ${pkgs.pamixer}/bin/pamixer --get-volume > ${wobfifo}
+    fi
+  '';
 in
 {
-  enviroment.systemPackages = [pkgs.wob brightness vol-control];
+
+  environment.systemPackages = [pkgs.wob brightness vol-control];
 
   services.udev.extraRules = ''
   ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="amdgpu_bl0", MODE="0666", RUN+="${pkgs.coreutils}/bin/chmod a+w /sys/class/backlight/amdgpu_bl0/brightness"
@@ -65,8 +67,8 @@ in
       # breaking my DE
       
       keybindings = lib.mkOptionDefault {
-        "XF86MonBrightnessUp"   = "exec ${brightness}/bin/brightness 1";
-        "XF86MonBrightnessDown" = "exec ${brightness}/bin/brightness -1";
+        "XF86MonBrightnessUp"   = "exec ${brightness}/bin/brightness.sh 1";
+        "XF86MonBrightnessDown" = "exec ${brightness}/bin/brightness.sh -1";
         "XF86AudioRaiseVolume" = "exec ${vol-control}/bin/vol-control i";
         "XF86AudioLowerVolume" = "exec ${vol-control}/bin/vol-control d";
         "XF86AudioMute"        = "exec ${vol-control}/bin/vol-control t";
