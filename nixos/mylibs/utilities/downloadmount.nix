116035345793d5d33dcae72191083293ae5a3189
volkswagenfeature <13547477+volkswagenfeature@users.noreply.github.com>
2024-04-11 13:41:43 -0400

    Moved downloads to /bulk/downloads-mountpoint/<user>/

--- nixos/mylibs/utilities/downloadmount.nix
+++ nixos/mylibs/utilities/downloadmount.nix
@@ -3,12 +3,15 @@ let
 
   dlPath = /bulk/downloads-mountpoint;
 
-  needsPorting= true;
+  # If you need to copy over a existing downloads folder, set to true
+  # Then copy manually.
+  needsPorting = false;
 
   realUsers = pkgs.lib.attrsets.filterAttrs 
     (n: v: v.createHome) 
     config.users.users;
 in
+{ 
   # Create <dlpath>/<user>/ for each user with a home directory
   systemd.tmpfiles.rules = (pkgs.lib.attrsets.mapAttrsToList 
     (k: v: 
@@ -16,9 +19,15 @@ in
       ''d ${path} 700 ${k} ${v.group} - -''
     )
     realUsers
-  ) + if needsPorting then [] else (
-    []
+  ) ++ ( if needsPorting then [] else 
+  # Symlink it to ~/Downloads
+    pkgs.lib.attrsets.mapAttrsToList 
+    (k: v: 
+      let path = builtins.toString (dlPath + "/${k}"); in 
+      ''L ${v.home}/Downloads - - - - ${path}''
+    )
+    realUsers
   );
 
-
+}
 
