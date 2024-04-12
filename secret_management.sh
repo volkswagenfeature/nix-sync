2e58e8e68be62db4b83ca4758540f5c774e645cb
volkswagenfeature <13547477+volkswagenfeature@users.noreply.github.com>
2024-04-11 20:37:32 -0400

    Added secret managment helper

---
+++ secret_management.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SECRETPATH="$(git rev-parse --show-toplevel)/nixos/secrets.nix"
+if [[$1 == "set"]] then
+       git add --intent-to-add $SECRETPATH
+       git update-index --assume-unchanged $SECRETPATH
+
+elif [[$1 == "unset"]] then
+       git update-index --force-remove $SECRETPATH
+else
+       echo "Doing Nothing." 
+fi
