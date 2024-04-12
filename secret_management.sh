bdbf57cf7aa57d5eb7ba598d81efb742c8207f35
volkswagenfeature <13547477+volkswagenfeature@users.noreply.github.com>
2024-04-11 20:41:52 -0400

    fixed secret_management

--- secret_management.sh
+++ secret_management.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+set -x
+
 SECRETPATH="$(git rev-parse --show-toplevel)/nixos/secrets.nix"
-if [[$1 == "set"]] then
+
+if [[ $1 == "set" ]] then
        git add --intent-to-add $SECRETPATH
        git update-index --assume-unchanged $SECRETPATH
 
-elif [[$1 == "unset"]] then
+elif [[ $1 == "unset" ]] then
        git update-index --force-remove $SECRETPATH
 else
        echo "Doing Nothing." 
