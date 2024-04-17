3f918dc372b6ff2bbc9a5ee3379c74859c2a00c0
volkswagenfeature <13547477+volkswagenfeature@users.noreply.github.com>
2024-04-17 19:38:48 -0400

    Version 2 of bisync script

--- nixos/mylibs/utilities/cloudsync.nix
+++ nixos/mylibs/utilities/cloudsync.nix
@@ -22,22 +22,23 @@
     });
 
     ### RCLONE SYNC JOB ###
+    /*
     casegen = check: var: value: ''
       ${check})
         ${var}="${value}"
         ;;
 
     '';
+    */
 
     # checkvar is the variable to be checked in the case
     # setvar is the one that gets instatiated
     # Cases is a attribute set with the format:
     # {checkval=string}
-    casegen = checkvar: setvar: cases:
+    casegen = checkvar: setvar: cases: default:
     let
       casestart = ''
       case ${checkvar} in
-
       '';
       caseopt = acc: check: value: acc+''
       #
@@ -46,33 +47,40 @@
           ;;
       '';
       caseend = ''
-
+        *)
+          ${default}
+          ;;
       esac
       '';
     in
       ( pkgs.lib.foldlAttrs caseopt casestart cases ) + caseend;
 
       remote-targets = casegen "$1" "PATHS" (
-        mapAttrs (k: v: " ${toString v.local} ${toString v.remote} ") sync_apps
-      );
+        builtins.mapAttrs (k: v: " ${toString v.local} ${toString v.remote} ") 
+        sync_apps
+      ) "echo no targetspec named $1; exit";
 
       modes = casegen "$2" "FLAGS" {
         firstrun = " --create-empty-src-dirs --resilient --resync -MvP ";
         sync = " --create-empty-src-dirs --resilient -MvP  ";
-      };
+      } "echo no mode definition for $2; exit";
 
 
-    rc-bis = pkgs.writeScriptBin "rc-bis" ''
-      ${casegen "$1" "PATHS" (mapAttrs (k: v: ' ${toString v.local} ${toString v.remote} ') sync_apps )}
+    bisync = pkgs.writeScriptBin "bisync" ''
+      # Select paths to the remote
+      ${remote-targets}
 
-      case $2 in
-        firstrun)
-          FLAGS=" --create-empty-src-dirs --resilient --resync -MvP "
-          ;;
-        sync)
-          FLAGS=" --create-empty-src-dirs --resilient -MvP  "
-        
-      esac
+      # Select rclone args
+      ${modes}
+
+      RCLONE_CMD="rclone bisync $PATHS $FLAGS ''${@:3}"
+      echo "Preparing to execute this bisync command:"
+      echo $RCLONE_CMD
+      read -p "Continue? [y/N] " confirm
+      if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]
+      then
+          exec $RCLONE_CMD
+      fi
     '';
 
   in
@@ -80,7 +88,7 @@
   environment.systemPackages = with pkgs; [
     nix-unstable.rclone
     ( pkgs.writeTextDir "rclone.conf" (iniGen config) )
-    rc-bis
+    bisync
   ];
 
 
