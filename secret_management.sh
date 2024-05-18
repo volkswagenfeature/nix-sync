9148b54cf45b9c7211cc0e01875f50b1a3629d97
volkswagenfeature <13547477+volkswagenfeature@users.noreply.github.com>
2024-05-17 23:05:44 -0400

    Hook development

--- secret_management.sh
+++ secret_management.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+GITPATH=$( git rev-parse --show-toplevel )
+mapfile SECRETPATH < <( find $GITPATH -name secrets.nix )
+if [[ "${#SECRETPATH[@]}" -ne 1 ]] then
+    echo "Error. More than one filepath with name 'secrets.nix' found."
+    for p in ${SECRETPATH[@]}; do echo ">" $p; done 
+    exit 1
+fi
+
 set -x
-
-SECRETPATH="$(git rev-parse --show-toplevel)/nixos/secrets.nix"
-
 if [[ $1 == "set" ]] then
-       git add --intent-to-add $SECRETPATH
-       git update-index --assume-unchanged $SECRETPATH
+    git add --intent-to-add $SECRETPATH
+    git update-index --assume-unchanged $SECRETPATH
 
 elif [[ $1 == "unset" ]] then
-       git update-index --force-remove $SECRETPATH
+    git update-index --force-remove $SECRETPATH
 else
-       echo "Doing Nothing." 
+    echo "Doing Nothing. Invalid option." 
 fi
