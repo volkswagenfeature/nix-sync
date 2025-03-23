b533137155fb2bff1f94b219e8936c0057a32b78
volkswagenfeature <13547477+volkswagenfeature@users.noreply.github.com>
2025-03-23 14:59:04 -0400

    Wireguard Conf Fix

--- nixos/mylibs/packsets/cloud-engineering.nix
+++ nixos/mylibs/packsets/cloud-engineering.nix
@@ -15,13 +15,29 @@ in
         enableOnBoot = false;
       };
       virtualisation.incus.enable = true;
+      systemd.services.incus.wantedBy = lib.mkForce [];
+
       virtualisation.lxd.enable = true;
       systemd.services.lxd.wantedBy = lib.mkForce [];
 
       # vpn configuration. Might belong in a different file?
 
       networking.wireguard.enable = true;
-      networking.wg-quick.interfaces.wg0.configFile = "/home/${secrets.primaryuser}/.ssh/wireguard.conf";
+      networking.wg-quick.interfaces.wg0 = {
+        autostart = false;
+        configFile = "/home/${secrets.primaryuser}/.ssh/wireguard.conf";
+      };
+      systemd.services.wg-quick-wg0 = { 
+        wantedBy = lib.mkForce ["graphical.target"];
+        after = [ "network-online.target" ];
+        wants = [ "network-online.target" ];
+      };
+      systemd.services.wireguard = {
+        wantedBy = lib.mkForce ["wg-quick-wg0.service"];
+        after = [ "network-online.target" ];
+        wants = [ "network-online.target" ];
+      };
+
         
 
       services.tailscale.enable = true;
