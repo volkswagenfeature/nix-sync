1e58fb1c3df8a968e94104805eab50e37eb6cfc8
volkswagenfeature <13547477+volkswagenfeature@users.noreply.github.com>
2025-05-07 15:56:09 -0400

    Added firewall rules for hydra

--- nixos/mylibs/utilities/hydraCI.nix
+++ nixos/mylibs/utilities/hydraCI.nix
@@ -8,4 +8,12 @@
     useSubstitutes = true;
     listenHost = "localhost";
   };
+  networking.firewall.extraInputRules = [
+    ''nft add chain inet myapp input { 
+      type filter hook input priority 0; 
+      policy accept; 
+    }''
+    "nft add rule inet myapp input iif lo tcp dport 3000 accept"
+    "nft add rule inet myapp input tcp dport 3000 drop"
+  ];
 }
