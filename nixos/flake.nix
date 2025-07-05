2fa55b5b95fffe6e497f5d239c9326647e5ebc08
volkswagenfeature <13547477+volkswagenfeature@users.noreply.github.com>
2025-07-05 17:21:58 -0400

    Updated to 25.05

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,16 +1,16 @@
 {
   inputs = {  
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nix-unstable-raw.url = "github:NixOS/nixpkgs/nixos-unstable";
     nix-razor-raw.url = "github:NixOS/nixpkgs/master";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nixvim = {
-      url = "github:/nix-community/nixvim/nixos-24.11";
+      url = "github:/nix-community/nixvim/nixos-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
       #inputs.flake-utils.follows = "flake-utils";
     };
@@ -33,7 +33,7 @@
     extra-args = { 
       defaults = rec {
         system = "x86_64-linux";
-        sysversion = "24.11";
+        sysversion = "25.05";
         pkgscon = {
           inherit system;
           config.allowUnfree = true;
