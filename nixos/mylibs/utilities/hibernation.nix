dddd0f635912205b373c366081769261fb9b6b9b
volkswagenfeature <13547477+volkswagenfeature@users.noreply.github.com>
2025-09-15 12:56:46 -0400

    typofix

--- nixos/mylibs/utilities/hibernation.nix
+++ nixos/mylibs/utilities/hibernation.nix
@@ -12,39 +12,41 @@ let
   };
   swapPartition = "/dev/disk/by-uuid/54d8eb27-9f0e-42b1-8457-2ec7f3577085";
 in {
-  config.boot.kernelParams = ["resume=${swapPartition}"];
-  config.boot.resumeDevice = swapPartition; 
+  config = {
+    boot.kernelParams = ["resume=${swapPartition}"];
+    boot.resumeDevice = swapPartition; 
 
-  systemd.services."awake-after-suspend-for-a-time" = {
-    description = "Sets up the suspend so that it'll wake for hibernation";
-    wantedBy = [ "suspend.target" ];
-    before = [ "systemd-suspend.service" ];
-    environment = hibernateEnvironment;
-    script = ''
-      ${pkgs.pipewire}/bin/pw-play ${hiberOnWake}
-      curtime=$(date +%s)
-      echo "$curtime $1" >> /tmp/autohibernate.log
-      echo "$curtime" > $HIBERNATE_LOCK
-      ${pkgs.utillinux}/bin/rtcwake -m no -s $HIBERNATE_SECONDS
-    '';
-    serviceConfig.Type = "simple";
-  };
-  systemd.services."hibernate-after-recovery" = {
-    description = "Hibernates after a suspend recovery due to timeout";
-    wantedBy = [ "suspend.target" ];
-    after = [ "systemd-suspend.service" ];
-    environment = hibernateEnvironment;
-    script = ''
-      ${pkgs.pipewire}/bin/pw-play ${hiberScriptRun}
-      curtime=$(date +%s)
-      sustime=$(cat $HIBERNATE_LOCK)
-      rm $HIBERNATE_LOCK
-      if [ $(($curtime - $sustime)) -ge $HIBERNATE_SECONDS ] ; then
-        systemctl hibernate
-      else
-        ${pkgs.utillinux}/bin/rtcwake -m no -s 1
-      fi
-    '';
-    serviceConfig.Type = "simple";
+    systemd.services."awake-after-suspend-for-a-time" = {
+      description = "Sets up the suspend so that it'll wake for hibernation";
+      wantedBy = [ "suspend.target" ];
+      before = [ "systemd-suspend.service" ];
+      environment = hibernateEnvironment;
+      script = ''
+        ${pkgs.pipewire}/bin/pw-play ${hiberOnWake}
+        curtime=$(date +%s)
+        echo "$curtime $1" >> /tmp/autohibernate.log
+        echo "$curtime" > $HIBERNATE_LOCK
+        ${pkgs.utillinux}/bin/rtcwake -m no -s $HIBERNATE_SECONDS
+      '';
+      serviceConfig.Type = "simple";
+    };
+    systemd.services."hibernate-after-recovery" = {
+      description = "Hibernates after a suspend recovery due to timeout";
+      wantedBy = [ "suspend.target" ];
+      after = [ "systemd-suspend.service" ];
+      environment = hibernateEnvironment;
+      script = ''
+        ${pkgs.pipewire}/bin/pw-play ${hiberScriptRun}
+        curtime=$(date +%s)
+        sustime=$(cat $HIBERNATE_LOCK)
+        rm $HIBERNATE_LOCK
+        if [ $(($curtime - $sustime)) -ge $HIBERNATE_SECONDS ] ; then
+          systemctl hibernate
+        else
+          ${pkgs.utillinux}/bin/rtcwake -m no -s 1
+        fi
+      '';
+      serviceConfig.Type = "simple";
+    };
   };
 }
